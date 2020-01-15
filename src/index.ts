659798c82f0639d52e07064c3f1ccb64303ca322
马加奥 <majiaao@spacebox.fun>
2020-01-15 14:25:33 +0800

    fix: 修复问题

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import commader from 'commander'
+import commader from "commander"
 import axios, { AxiosResponse } from "axios"
-import colors from 'colors'
-import Config from './config/config'
-import Url from './util/url'
-
+import colors from "colors"
+import Config from "./config/config";
+import Url from "./util/url";
 const log = console.log;
 interface ILive {
         province: string;
@@ -25,19 +24,23 @@ interface IWeatherResponse {
 }
 commader.version('1.0.0')
         .option("-c --city [cityName]", "Enter the city for the weather query")
+        .option("-t --query-type", "Entery query weather type")
         .parse(process.argv);
-if (commader.city) {
-        axios.get(`${Url.baseUrl}?city=${encodeURI(commader.city)}&key=${Config.KEY}`).then((res: AxiosResponse<IWeatherResponse>) => {
-                console.log(res.data)
-                if (res.data.status == '1') {
-                        let {city, weather, temperature, winddirection, windpower, reporttime} = res.data.lives[0]
-                        log(colors.green('天气查询成功 :)'));
-                        log(colors.green(`${city}`));
-                        log(colors.green(`天气: ${weather}, 温度${+temperature}度`));
-                        log(colors.green(`${winddirection}风${windpower}级`));
-                        log(colors.green(`${reporttime}`))
-                }else {
-                        log(colors.red('opps~ 天气查询失败 :('))
-                }
-        })
+if (process.argv.slice(2).length === 0) {
+        commader.outputHelp();
+        process.exit();
 }
+axios.get(`${Url.baseUrl}?city=${encodeURI(commader.city)}&key=${Config.KEY}&extensions=${encodeURI(commader.queryType ? "base" : "all")}`).then((res: AxiosResponse<IWeatherResponse>) => {
+        if (res.data.status === "1") {
+                let { city, weather, temperature, winddirection, windpower, reporttime } = res.data.lives[0];
+                log(colors.green("天气查询成功 :)"));
+                log(colors.green(`${city}`));
+                log(colors.green(`天气: ${weather}, 温度${+temperature}度`));
+                log(colors.green(`${winddirection}风${windpower}级`));
+                log(colors.green(`${reporttime}`));
+        } else {
+                log(colors.red("opps~ 天气查询失败 :("))
+        }
+}).catch(() => {
+        log(colors.red("opps~ 天气查询失败 :("));
+});
